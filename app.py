c0be27fbcb3daa083f16aea6bff1020217b8d8dc
RidhiForge <ridhi25102007@gmail.com>
2025-10-25 14:16:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ APP_LINK = "https://unreverberated-melodie-prevailingly.ngrok-free.dev/#probabil
 
 st.set_page_config(page_title="Financial News Sentiment Analyzer", page_icon="💹", layout="centered")
 
-st.title("💹 Financial News Sentiment Analyzer App")
+st.title(" Financial News Sentiment Analyzer App")
 st.write("Analyze financial headlines and get sentiment predictions powered by FinBERT (ProsusAI/finbert).")
 
 st.markdown(
     f"""
-    🌐 **Live App:** [👉 Open Hosted Version]({https://unreverberated-melodie-prevailingly.ngrok-free.dev/#probabilities})
+     **Live App:** [ Open Hosted Version]({https://unreverberated-melodie-prevailingly.ngrok-free.dev/#probabilities})
     """,
     unsafe_allow_html=True
 )
